ebd252a2fcb3ad11c1e5318cabff15185880bbd3
samuel@SudoSu <samuel_mesquita@estudante.sesisenai.org.br>
2026-06-15 21:59:45 -0300

    feat: Modificacao do pacienteRequest e MedicoController, e criacao e possivel finalizacao do paciente = service, repository e controlller

--- backend/AegisPrev/src/main/java/com/sa/AegisPrev/repository/PacienteRepository.java
+++ backend/AegisPrev/src/main/java/com/sa/AegisPrev/repository/PacienteRepository.java
@@ -3,5 +3,15 @@ package com.sa.AegisPrev.repository;
 import com.sa.AegisPrev.models.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    List<Paciente> findByNomePacienteContainingIgnoreCase (String nomePaciente);
+
+    Optional<Paciente> findByCpfPacienteContainingIgnoreCase (String cpfPaciente);
+
+    List<Paciente> findByDataNascimento (LocalDate dataNascimento);
 }
