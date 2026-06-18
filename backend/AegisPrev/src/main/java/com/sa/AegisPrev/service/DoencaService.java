b4f42197373b10303dd2a7ecf026c719862d420e
samuel@SudoSu <samuel_mesquita@estudante.sesisenai.org.br>
2026-06-17 21:08:32 -0300

    feat: Sla o que eu fiz

--- backend/AegisPrev/src/main/java/com/sa/AegisPrev/service/DoencaService.java
+++ backend/AegisPrev/src/main/java/com/sa/AegisPrev/service/DoencaService.java
@@ -1,6 +1,7 @@
 package com.sa.AegisPrev.service;
 
 import com.sa.AegisPrev.DTO.DoencaResponseDTO;
+import com.sa.AegisPrev.DTO.SintomaResumoDTO;
 import com.sa.AegisPrev.models.Doenca;
 import com.sa.AegisPrev.repository.DoencaRepository;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,16 @@ public class DoencaService {
     }
 
     public DoencaResponseDTO toResponseDTO(Doenca doenca){
-        return new DoencaResponseDTO(/*inserir os valores do DTO*/);
+        return new DoencaResponseDTO(
+                doenca.getIdDoenca(),
+                doenca.getNomeDoenca(),
+                doenca.getDescricaoDoenca(),
+                doenca.getHereditaria(),
+                doenca.getSintomas().stream().map(sintoma -> new SintomaResumoDTO(
+                        sintoma.getIdSintoma(),
+                        sintoma.getNomeSintoma()
+                )).toList()
+        );
     }
 
 
