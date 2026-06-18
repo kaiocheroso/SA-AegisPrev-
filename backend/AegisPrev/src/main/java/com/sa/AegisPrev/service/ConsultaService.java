e21258add6767d3fccea8ca9098320bfd98f8971
samuel@SudoSu <samuel_mesquita@estudante.sesisenai.org.br>
2026-06-17 22:22:52 -0300

    feat: criando service de consulta

--- backend/AegisPrev/src/main/java/com/sa/AegisPrev/service/ConsultaService.java
+++ backend/AegisPrev/src/main/java/com/sa/AegisPrev/service/ConsultaService.java
@@ -10,9 +10,13 @@ import com.sa.AegisPrev.repository.PacienteRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ConsultaService {
+    /// RAPAZIADA, TEREMOS QUE CRIAR O SINTOMA e fazer o resultado trazer com lista de possiveis doencas
+    /// teremos que pegar os sintomas pelo paciente, e fazer com que as possiveis doencas aparecam
+
     private final ConsultaRepository repository;
     private final MedicoRepository medicoRepository;
     private final PacienteRepository pacienteRepository;
@@ -22,20 +26,6 @@ public class ConsultaService {
         this.repository = repository;
         this.medicoRepository = medicoRepository;
         this.pacienteRepository = pacienteRepository;
-    }
-
-    public ConsultaResponseDTO salvar(ConsultaRequest dto){
-        Medico medico = medicoRepository.findById(dto.idMedico()).orElseThrow(() -> new RecursoNaoEncontradoException("ID do medico nao encontrado"));
-        Paciente paciente = pacienteRepository.findById(dto.idPaciente()).orElseThrow(() -> new RecursoNaoEncontradoException("ID do paciente nao encontrado"));
-        Consulta consulta = new Consulta();
-
-        consulta.setMedico(medico);
-        consulta.setPaciente(paciente);
-        consulta.setDataConsulta(LocalDateTime.now()); //talvez tenha que apagar o date do consultaRequest
-        consulta.setDescricao(dto.descricao());
-
-        Consulta salvo = repository.save(consulta);
-        return toResponse(salvo);
     }
 
     private ConsultaResponseDTO toResponse(Consulta consulta){
@@ -57,6 +47,36 @@ public class ConsultaService {
                         doenca.getNomeDoenca()
                 )).toList()
         );
+    }
+
+    public List<ConsultaResponseDTO> listar(){
+        return repository.findAll().stream().map(this::toResponse).toList();
+    }
+
+    public ConsultaResponseDTO buscarPorId(Long id){
+        return toResponse(pegarId(id));
+    }
+
+    private Consulta pegarId(Long id){
+        return repository.findById(id).orElseThrow(() -> new RecursoNaoEncontradoException("ID não encontrado"));
+    }
+
+    public ConsultaResponseDTO salvar(ConsultaRequest dto){
+        Medico medico = medicoRepository.findById(dto.idMedico()).orElseThrow(() -> new RecursoNaoEncontradoException("ID do medico nao encontrado"));
+        Paciente paciente = pacienteRepository.findById(dto.idPaciente()).orElseThrow(() -> new RecursoNaoEncontradoException("ID do paciente nao encontrado"));
+        Consulta consulta = new Consulta();
+
+        consulta.setMedico(medico);
+        consulta.setPaciente(paciente);
+        consulta.setDataConsulta(LocalDateTime.now()); //talvez tenha que apagar o date do consultaRequest
+        consulta.setDescricao(dto.descricao());
+
+        Consulta salvo = repository.save(consulta);
+        return toResponse(salvo);
+    }
+
+    public ConsultaResponseDTO atualizar(ConsultaRequest dto){
+
     }
 
 
