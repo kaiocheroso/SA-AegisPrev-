f5110a1e1a12f3560389ad58166475f828b992a3
Pingu-BR <gtjgujggugg@gmail.com>
2026-06-22 19:03:38 -0300

    falta o buscarPorId e o listar

--- backend/AegisPrev/src/main/java/com/sa/AegisPrev/controller/DoencaController.java
+++ backend/AegisPrev/src/main/java/com/sa/AegisPrev/controller/DoencaController.java
@@ -1,4 +1,34 @@
 package com.sa.AegisPrev.controller;
 
+import com.sa.AegisPrev.DTO.DoencaRequestDTO;
+import com.sa.AegisPrev.DTO.DoencaResponseDTO;
+import com.sa.AegisPrev.service.DoencaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doencas")
 public class DoencaController {
+
+    private final DoencaService service;
+
+    public DoencaController(DoencaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public DoencaResponseDTO salvar(@RequestBody DoencaRequestDTO dto) {
+        return service.salvar(dto);
+    }
+
+    @PutMapping("/{id}")
+    public DoencaResponseDTO atualizar(@PathVariable Long id, @RequestBody DoencaRequestDTO dto) {
+        return service.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
